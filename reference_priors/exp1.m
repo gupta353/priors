64a4389b7924cf08ca4da65ea77a8dc2b3ce8c1c
unknown <gupta353@purdue.edu>
2019-06-06 19:07:01 +0100

    comments added, numrical implementation of Berger and Bernardo (2009) algorithm to compute reference prior

--- reference_priors/exp1.m
+++ reference_priors/exp1.m
@@ -1,31 +1,32 @@
-%% referefence priors for Normal distribution scale
+% Numerical computation of referefence priors for Normal distribution scale
+% (standard deviation)
+% reference: Berger et al. (2009)
+% alpha=frac_{\sum_{i=1}{k}(x_i-\mu)}{2}
+% initial prior, \pi^*=1
+% 
 
 clear all
 close all
 clc
 
-k=100;
-m=1000;
-mu=0;
+k=1000;          % number of samples sets to be drawn
+m=10000;         % number of samples in each set
+mu=10;           % known mean of the normal distribution
 
-pi_star=1;
+pi_star=1;      % inital prior
 
-sig2=1:10;
+sig2=1:0.1:10;      % vriance values at which prior has to be computed
 
 for i=1:length(sig2)
+    
     for j=1:m
         sig2_tmp=sig2(i);
-        samps=normrnd(mu,sig2_tmp,[k,1]);
-        func=@(x)prod(integral_normpdf(samps,ones(length(samps),1)*mu,x));
-        c(j)=integral(func,1,10);
-        % integration using trapz method
-%         x=0.1:0.1:20;
-%         for l=1:length(x)
-%             y(l)=func(x(l));
-%         end
-%         c(j)=trapz(x,y);
-        expr=prod(normpdf(samps,mu,sig2_tmp))*pi_star/c(j);
-        r(j)=log(expr);
+        samps=normrnd(mu,sqrt(sig2_tmp),[k,1]);     % samples from normal distribution
+        alpha=sum((samps-mu).^2)/2;                 % alpha defined in header
+        log_asymp_post(j)=(k/2-1/2)*log(alpha)-...  % log of asymptotic posterior
+            k*log(sig2_tmp)/2-gammaln(k/2-1/2)-alpha/sig2_tmp;
+        
     end
-    PI(i)=exp(sum(r)/m);
+    PI(i)=exp(mean(log_asymp_post));    % prior distribution function
+    
 end
