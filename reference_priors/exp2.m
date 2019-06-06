1fbf3bd2646ac11d6cca19cce598f1f9909539e8
unknown <gupta353@purdue.edu>
2019-06-06 20:11:33 +0100

    comments added to exp2, numrical implementation of Berger and Bernardo (2009) algorithm to compute reference prior

--- reference_priors/exp2.m
+++ reference_priors/exp2.m
@@ -4,28 +4,32 @@ clear all
 close all
 clc
 
-k=100;
-m=10000;
-sig2=1;
+k=1000;          % number of samples in each set
+m=100000;        % number of sets of samples
+sig2=1;         % known variance 
 
-pi_star=1;
+pi_star=1;      % intial prior
 
-mu=-10:10;
+mu=-10:0.1:10;      % values of mu at which prior function is to be evaluated
 
 for i=1:length(mu)
+    
     for j=1:m
         mu_tmp=mu(i);
-        samps=normrnd(mu_tmp,sig2,[k,1]);
-        func=@(x)prod(normpdf(samps,x,sig2));
-%         c(j)=integral(func,1,10);
+        samps=normrnd(mu_tmp,sqrt(sig2),[k,1]); % k samples drawn from normal distribution
+        avg=sum(samps)/k;                       % sample average of drawn samples
+        log_asymp_post(j)=0.5*log(k)-0.5*log(2*pi)...
+            -log(sig2)/2-k*(mu_tmp-avg)^2/2/sig2;
+        
         % integration using trapz method
-        x=-20:0.1:20;
-        for l=1:length(x)
-            y(l)=func(x(l));
-        end
-        c(j)=trapz(x,y);
-        expr=prod(normpdf(samps,mu_tmp,sig2))*pi_star/c(j);
-        r(j)=log(expr);
+%         x=-20:0.1:20;
+%         for l=1:length(x)
+%             y(l)=func(x(l));
+%         end
+%         c(j)=trapz(x,y);
+%         expr=prod(normpdf(samps,mu_tmp,sig2))*pi_star/c(j);
+%         r(j)=log(expr);
     end
-    PI(i)=exp(sum(r)/m);
+    
+    PI(i)=exp(sum(log_asymp_post)/m);
 end
