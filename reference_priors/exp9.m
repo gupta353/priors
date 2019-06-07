c39a6ff91a523fd868232763c5d8de495e7a7343
unknown <gupta353@purdue.edu>
2019-06-07 18:06:28 +0100

    reference and problem information added in the header, comments addeds in the header

--- reference_priors/exp9.m
+++ reference_priors/exp9.m
@@ -1,6 +1,12 @@
 % numerical implementation of Beranrdo's algorithm to determine hydraulic-condutivity 
 % assuming Green-Ampt infiltration model to be a true model
 % \alpha=\frac_{\sum_{i=1}{k}(x_i-\mu)^2/}{2*\sigma^2}
+% initial prior used: pi_star=\partialg/\partial\theta
+% model: x=g(\theta)+\epsilon, \epsilon~N(0,\sigma^2), g is the Green-Ampt
+% model
+% references: Berger et al. (2009), The formal definition of reference
+% priors
+% Chow et al. (1988), Applied Hydrology
 
 clear all
 close all
@@ -15,8 +21,8 @@ delta_theta=0.340;  % change in moisture content
 t=3600;   % time at which infiltration is computed (in s)
 g=@(x)Green_Ampt_solution(x,psi,delta_theta,t);
 
-kh=(1:0.1:10)/3600;          % hydraulic conductivity values at which prior is to be evaluated
-sig2=100;                  % variance values at which prior is to be evaluated
+kh=(1:0.5:30)/3600;          % hydraulic conductivity values at which prior is to be evaluated
+sig2=[1,10];                  % variance values at which prior is to be evaluated
 
 for i=1:length(kh)
     for ii=1:length(sig2)
@@ -31,10 +37,10 @@ for i=1:length(kh)
             alpha=sum((samps-mu_tmp).^2)/2;                 % defined in the header
             der=Green_Ampt_der(K,psi,delta_theta,t);
             
-            log_asymp_post(j)=(k-2)*log(s2)/2+(k-1)/2*log(k)-...
-                0.5*log(2*pi)-k*log(sig2)/2+(k-4)/2*log(2)-...
-                gammaln(k/2-1)-alpha/sig2+log(der);
+            log_asymp_post(j)=(k-2)*log(s2)/2+(k-1)/2*log(k)-...        % log of asymptotic posterior at the given point in parameter space
+                0.5*log(2*pi)-k*log(sig2_tmp)/2+(k-4)/2*log(2)-...
+                gammaln(k/2-1)-alpha/sig2_tmp+log(der);
         end
-        PI(i,ii)=exp(sum(log_asymp_post)/m);
+        PI(i,ii)=exp(sum(log_asymp_post)/m);                            % evaluation of the prior at the given point in parameter space
     end
 end
