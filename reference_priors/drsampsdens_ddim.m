3e10cf89344b90571b3736e32a799bbe04cb4039
unknown <gupta353@purdue.edu>
2019-06-28 05:06:46 +0100

    transpose operator removed from sep and transpoter operator put on u2 in line 59 and 69; also the codes has been tested multivariate Normal distribution

--- reference_priors/drsampsdens_ddim.m
+++ reference_priors/drsampsdens_ddim.m
@@ -6,18 +6,26 @@ clear all
 close all
 clc
 
-x=[-4:0.1:4]';
-pdf=normpdf(x);
+x1=-4:0.1:4;
+x2=-4:0.1:4;
+count=0;
+for i=1:length(x1)
+    for j=1:length(x2)
+        count=count+1;
+        x(count,:)=[x1(i),x2(j)];
+    end
+end
+pdf=mvnpdf(x,[0,0],[1,0;0,1]);
 
 n=10000;                  % number of samples to be drawn
 [ns,d]=size(x);         % ns=number of points at which density is available; d=number of dimensions
 
-sep=x(2)-x(1);
+sep=[0.1,0.1];
 base_area=prod(sep);
 
-bpoints=[-4,4];
+bpoints=[-4,4;-4,4];
 bpoints=reshape(bpoints,1,numel(bpoints));
-diff=repmat(x,1,2*d)-repmat(bpoints,size(x,1),1);
+diff=repmat(x,1,2)-repmat(bpoints,size(x,1),1);
 % replace zeros in in first d-columns of diff by 1 to indicate a lower boundary point 
 bpoints=diff(:,1:d)==0;
 % replace zeros in in last d-columns of diff by -1 to indicate an upper boundary point
@@ -48,7 +56,7 @@ for i=1:length(segment)
     temp_seg=segment(i);
     if num_bpoints(temp_seg)==0
         u2=unifrnd(-0.5,0.5,d,1);
-        y(i,:)=x(temp_seg,:)+sep'*u2;
+        y(i,:)=x(temp_seg,:)+sep.*u2';
     else
         ind_low=find(bpoints(temp_seg,:)==1);
         ind_up=find(bpoints(temp_seg,:)==-1);
@@ -58,7 +66,7 @@ for i=1:length(segment)
         unif_up_lim(ind_up-d)=0;
         
         u2=unifrnd(unif_low_lim,unif_up_lim);
-        y(i,:)=x(temp_seg,:)+sep'*u2;
+        y(i,:)=x(temp_seg,:)+sep.*u2';
     end
 end
 
