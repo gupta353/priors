2fb0f1b476e276d3eb7268b2461a5770aefcfb02
unknown <gupta353@purdue.edu>
2019-06-30 19:19:40 +0100

    first addition: computes derivative of Green-Ampt equation at time t

---
+++ reference_priors/Green_Ampt_der.m
@@ -0,0 +1,14 @@
+% Computation of derivative of Green-Ampt model w/r/t hydraulic conductivity
+% Inputs: K=Hydraulic conductivity (in cm s^-1)
+%         t=time at which infiltration is to be computed (in seconds)
+%         psi=suction head (cm)
+%         delta_theta=porosity-intial mositure content
+% Outputs: der=Derivative of cumulative-infiltration w/r/t hydraulic
+%          conductivity at input K
+
+function der=Green_Ampt_der(K,psi,delta_theta,t)
+
+Ft=Green_Ampt_solution(K,psi,delta_theta,t);
+der=t.*(Ft./psi/delta_theta+1);
+
+end
