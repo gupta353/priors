98d82ff3125a4f9a04638e6696aac663bf8de817
unknown <gupta353@purdue.edu>
2019-07-05 16:55:46 +0100

    first addition: code for the computation of marginal pdf of sigma at a given point in parameter space

---
+++ reference_priors/marginal_bernardo_pdf_sigma.m
@@ -0,0 +1,21 @@
+% computation of marginal pdf of sigma from the joint Bernardo-pdf
+
+
+function marginal_pdf_x=marginal_bernardo_pdf_sigma(x)
+
+global GLOBAL_DATA
+marginal_pdf_values=GLOBAL_DATA.marginal_pdf_values;
+sigma_range=GLOBAL_DATA.sigma_range;
+
+% compute marginal pdf at user-given x
+diff=repmat(x,length(marginal_pdf_values),1)-sigma_range;
+diff=abs(diff);
+ind=find(diff==min(diff));
+
+if length(ind)>1
+    marginal_pdf_x=randomsample(marginal_pdf_values(ind),1);
+else
+    marginal_pdf_x=marginal_pdf_values(ind);
+end
+
+end
