771867a38fc80092c2d0b67b32a79aa56f2309aa
unknown <gupta353@purdue.edu>
2019-07-05 19:07:06 +0100

    variable names chnaged

--- reference_priors/marginal_bernardo_pdf_sigma.m
+++ reference_priors/marginal_bernardo_pdf_sigma.m
@@ -4,18 +4,18 @@
 function marginal_pdf_x=marginal_bernardo_pdf_sigma(x)
 
 global GLOBAL_DATA
-marginal_pdf_values=GLOBAL_DATA.marginal_pdf_values;
+marginal_pdf_values_sigma=GLOBAL_DATA.marginal_pdf_values_sigma;
 sigma_range=GLOBAL_DATA.sigma_range;
 
 % compute marginal pdf at user-given x
-diff=repmat(x,length(marginal_pdf_values),1)-sigma_range;
+diff=repmat(x,length(marginal_pdf_values_sigma),1)-sigma_range;
 diff=abs(diff);
 ind=find(diff==min(diff));
 
 if length(ind)>1
-    marginal_pdf_x=randomsample(marginal_pdf_values(ind),1);
+    marginal_pdf_x=randomsample(marginal_pdf_values_sigma(ind),1);
 else
-    marginal_pdf_x=marginal_pdf_values(ind);
+    marginal_pdf_x=marginal_pdf_values_sigma(ind);
 end
 
 end
