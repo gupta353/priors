2cbebe42437f8e629018cc1ccadf02951c2bdc77
unknown <gupta353@purdue.edu>
2019-08-15 05:19:47 +0100

    first addition: a script to compute cumulative infiltration fot falling head infoltrometer data

---
+++ reference_priors/falling_head_Green_Ampt_solution.m
@@ -0,0 +1,39 @@
+% Iterative solution to Green-Ampt Equation
+% Inputs: K=Hydraulic conductivity (in cm s^-1)
+%         t=time at which infiltration is to be computed (in seconds)
+%         psi=suction head (cm)
+%         delta_theta=porosity-intial mositure content
+%         H0=intial water level height from the surface (cm)
+% Outputs: Ft=cumulative infiltration at time t (cm)
+
+function Ft=falling_head_Green_Ampt_solution(K,psi,delta_theta,H0,t)
+
+tol=0.000001;   % convregence-error tolerance 
+a=1-delta_theta;
+b=(H0+psi)*delta_theta;
+
+if length(t)==1 % if t is a scalar
+    
+    Ft_old=-inf;
+    Ft_updated=a*K*t;
+
+    while abs(Ft_updated-Ft_old)>tol
+        Ft_old=Ft_updated;
+        Ft_updated=a*K*t+(b/a)*log(1+a*Ft_old/b);
+    end
+
+    Ft=Ft_updated;
+    
+else  % it t is a vector
+    
+    Ft_old=-inf*length(t);
+    Ft_updated=a*K*t;
+    
+    while max(abs(Ft_updated-Ft_old))>tol
+        Ft_old=Ft_updated;
+        Ft_updated=a*K*t+(b/a)*log(1+a*Ft_old/b);
+    end
+    Ft=Ft_updated;
+end
+
+end
