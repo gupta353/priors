440b4b62480533e8fcccb087a525c0fcae9682ab
unknown <gupta353@purdue.edu>
2019-08-29 18:20:23 +0100

    modified for falling head Green-Ampt method

--- reference_priors/Gaussloglikeli.m
+++ reference_priors/Gaussloglikeli.m
@@ -11,15 +11,15 @@ global GLOBAL_DATA
 
 y0=GLOBAL_DATA.y0;
 % other known parameters of Green-Ampt equation
-psi=16.68;          % (in cm)
-delta_theta=0.340;  % change in moisture content
-t=3600;   % time at which infiltration is computed (in s)
-g=@(x)Green_Ampt_solution(x,psi,delta_theta,t);
+psi=50;          % (in cm)
+delta_theta=0.004787;  % change in moisture content
+t=1148;   % time at which infiltration is computed (in s)
+H0=13.7;
 
 kh=theta(1);        % hydraulic conductivity in cm s^-1
 sig=theta(2);       % standard-deviation
 
-mu=g(kh);           % computation of cumulative infiltration at kh
+mu=falling_head_Green_Ampt_solution(kh,psi,delta_theta,H0,t);           % computation of cumulative infiltration at kh
 
 L=-0.5*log(2*pi)-log(sig)-(y0-mu)^2/2/sig^2; % computation of log-likelihood
 
