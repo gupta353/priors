ee97575c38d773b3a9506751dd3af92b0052bf2a
unknown <gupta353@purdue.edu>
2020-08-18 16:10:12 +0100

    computations of hessian of a functional of two variables added

--- reference_priors/jacobiancomp_func/hessiancomp.m
+++ reference_priors/jacobiancomp_func/hessiancomp.m
@@ -2,9 +2,17 @@
 
 function hess = hessiancomp(fun,a,h)
       
-    % computation of second derivatie (three point-method)
+    % computation of second derivatie of a one-dimenisonal function(three point-method)
     % der2 = (fun(x+h) + fun(x-h) - 2*fun(x))/h^2;
     
-    % computation of second derivatie (four-point method)
-    hess = (-fun(a-2*h) + 16*fun(a-h) - 30*fun(a) + 16*fun(a+h) - fun(a+2*h))/12/h^2;
+    % computation of second derivatie of a one-dimensional function (four-point method)
+%     hess = (-fun(a-2*h) + 16*fun(a-h) - 30*fun(a) + 16*fun(a+h) - fun(a+2*h))/12/h^2;
+    
+    % computation of second derivative matrix (Hessian) of a two-dimensional function (three point method for pure derivative and central difference method for mixed derivatives)
+    hess(1,1) = (fun(a) - 2*fun(a+[h,0]) + fun(a+[2*h,0]))/h^2;
+    hess(2,2) = (fun(a) - 2*fun(a+[0,h]) + fun(a+[0,2*h]))/h^2;
+    hess(1,2) = (fun(a+[h,h]) + fun(a+[-h,-h]) - fun(a+[-h,h]) - fun(a+[h,-h]))/4/h^2;
+    hess(2,1) = hess(1,2);
+    
 end
+
