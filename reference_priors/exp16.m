fbc44779b715d87f0ff8f7b2037082df74a143af
unknown <gupta353@purdue.edu>
2020-12-24 19:57:46 +0000

    number of training samples increased for accurate prediction in the region 10 (Note: this is the the third run for the latest refinement of the predictor space)

--- reference_priors/exp16.m
+++ reference_priors/exp16.m
@@ -379,8 +379,8 @@ save(filename,'Xtrain','ytrain');
 %}
 
 %% create training data for GPR uisng sobolev sequences
-n_sobol_samps = 470;     % number of latin hypercube samples on each dimension
-sample_interval = 43000+1:43000+n_sobol_samps;
+n_sobol_samps = 1130;     % number of latin hypercube samples on each dimension
+sample_interval = 46000+1:46000+n_sobol_samps;
 n_var = 5;              % number of dimensions (each dimension corresponds to one parameter)
 rng(1);
 
