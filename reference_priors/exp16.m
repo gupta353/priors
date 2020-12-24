23a6df0a2ae9ec2aaac9aaba91cc8ca960d0f5e4
unknown <gupta353@purdue.edu>
2020-12-24 20:17:58 +0000

    number of training samples increased for accurate prediction in the region 11

--- reference_priors/exp16.m
+++ reference_priors/exp16.m
@@ -379,15 +379,15 @@ save(filename,'Xtrain','ytrain');
 %}
 
 %% create training data for GPR uisng sobolev sequences
-n_sobol_samps = 1130;     % number of latin hypercube samples on each dimension
-sample_interval = 46000+1:46000+n_sobol_samps;
+n_sobol_samps = 409;     % number of latin hypercube samples on each dimension
+sample_interval = 48000+1:48000+n_sobol_samps;
 n_var = 5;              % number of dimensions (each dimension corresponds to one parameter)
 rng(1);
 
 par_range = [1, 2000;...                      % storage capacity (in mm)
-    0.1, 0.2;...                              % storage distribution parameter
+    0.12, 0.2;...                              % storage distribution parameter
     -15, -5;...                               % logarithm of baseflow reservior constant (in s^-1)
-    2, 10;...                              % in-stream-velocity (in m s^-1)
+    0.01, 2;...                              % in-stream-velocity (in m s^-1)
     0.01, 10];                                % hill slope velocity (in m s^-1)
 
 param_samps = SobolsampAG(n_sobol_samps,n_var,par_range,sample_interval);
