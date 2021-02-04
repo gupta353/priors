07e9388c71c4be00571f70278b2328f0fa6ea20d
unknown <gupta353@purdue.edu>
2021-02-04 17:47:40 +0000

    script to draw samples from region 9 (this is a re-run for region 9)

--- reference_priors/exp16.m
+++ reference_priors/exp16.m
@@ -380,8 +380,8 @@ save(filename,'Xtrain','ytrain');
 
 %% create training data for GPR uisng sobolev sequences
 %
-n_sobol_samps = 2000;     % number of latin hypercube samples on each dimension
-sample_interval = 130000+1:130000+n_sobol_samps;
+n_sobol_samps = 1000;     % number of latin hypercube samples on each dimension
+sample_interval = 140000+1:140000+n_sobol_samps;
 n_var = 5;              % number of dimensions (each dimension corresponds to one parameter)
 rng(1);
 
